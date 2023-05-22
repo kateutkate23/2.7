42b1734dc1d6cd6dd2363fc47156bcd28bd2cf01
kate_utkate <kate_utkate@KATE-UTKATE>
2023-05-22 16:31:41 +0300

    Добавьте файлы проекта.

---
+++ Module2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace Module2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Уткина Екатерина Алексеевна";
+            sbyte age = 18;
+            string email = "kate_utkate@vk.com";
+            double progPoints = 9.4;
+            double mathPoints = 8.5;
+            double physPoints = 3.7;
+
+            string pattern = "ФИО: {0}\nВозраст: {1}\nПочта: {2}\nПрограммирование: {3}\nМатематика: {4}\nФизика: {5}";
+
+            WriteLine(pattern, fullName, age, email, progPoints, mathPoints, physPoints);
+            ReadKey();
+
+            double total = progPoints + mathPoints + physPoints;
+            double avg = total / 3;
+
+            WriteLine($"Средний балл по трем предметам: {avg}");
+            ReadKey();
+        }
+    }
+}
